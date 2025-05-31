9596bc7a0184cca38fe8275b2a1319da351a2941
Leo <leopldu45@gmail.com>
2025-05-31 20:02:47 +0200

    glouton KP

--- src/KP/Local/Construction.cpp
+++ src/KP/Local/Construction.cpp
@@ -9,23 +9,20 @@ using namespace std;
 Sol construction(Problem problem) {
  	Sol sol;
 
-	vector<float> U(problem.size,0);
+	vector<pair<float,int>> U(problem.size);
     for(int i=0;i<problem.size;i++) {
-		U[i]=problem.coef[i]/problem.poids[i];
+		U[i]={problem.coef[i]/problem.poids[i],i};
 
     }
-	sort(U.begin(),U.end(),compar_decroissant<float>);
+	sort(U.begin(),U.end(),compar_decroissant<pair<float,int>>);
 
     vector<int> solution(problem.size,0);
 
-	//pb il faut conserver l'indice
-	while (i<problem.size && ) {
 
-	}
     for(int i=0;i<problem.size;i++) {
-        if(problem.poids_courant+U[i]<problem.capacite) {
-            problem.poids_courant+=U[i];
-            solution[i]=1;
+        if(problem.poids_courant+U[i].first<problem.capacite) {
+            problem.poids_courant+=U[i].first;
+            solution[U[i].second]=1;
         }
     }
 
