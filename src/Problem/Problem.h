8ecd54ffc7f3f696a29375810ee96d33f7f52c48
Leo <leopldu45@gmail.com>
2025-06-01 15:28:56 +0200

    glouton KP

---
+++ src/Problem/Problem.h
@@ -0,0 +1,19 @@
+//
+// Created by Leo on 01/06/2025.
+//
+#include<string>
+
+#ifndef PROBLEM_H
+#define PROBLEM_H
+
+
+
+class Problem {
+    public:
+        string to_string();
+    private:
+};
+
+
+
+#endif //PROBLEM_H
