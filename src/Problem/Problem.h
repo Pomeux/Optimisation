1d5216e8bbc1222e8f22598569386a7f9ef59724
Leo <leopldu45@gmail.com>
2025-06-03 19:28:12 +0200

    problem

--- src/Problem/Problem.h
+++ src/Problem/Problem.h
@@ -2,6 +2,9 @@
 // Created by Leo on 01/06/2025.
 //
 #include<string>
+#include<vector>
+
+using namespace std;
 
 #ifndef PROBLEM_H
 #define PROBLEM_H
@@ -11,7 +14,18 @@
 class Problem {
     public:
         string to_string();
+        double get_z();
+        double caculate_z();
+        int get_nb_variable();
+        int get_nb_constrainte();
+		vector<vector<double> > get_contrainte();
+
     private:
+      double z;
+      int contrainte;
+      int variable;
+      vector<vector<double> > constraintes;
+
 };
 
 
